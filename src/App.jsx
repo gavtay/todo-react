53e432fef63474108d622dea9a6678e1a3bdcbff
Kataneki <gavintaylor.ai@gmail.com>
2023-11-06 15:45:51 -0600

    Deleted all unused files because combined program to one component. Fixed the complete functionality. Cleaned up code in several sections. Added a word count for input, added css.

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCheck } from '@fortawesome/free-solid-svg-icons'
 import { faX } from '@fortawesome/free-solid-svg-icons'
-import { useEffect, useState } from 'react'
-import './task.css'
-import './create.css'
-import './task.css'
+import { useState } from 'react'
 import './App.css'
 
 const checkBtn = <FontAwesomeIcon id="check" icon={faCheck} style={{color: "#00ff00", pointerEvents: 'none'}} />
 const xBtn = <FontAwesomeIcon id="x" icon={faX} style={{color: "#ff0000", pointerEvents: 'none'}} />
 
-
-
 export default function App() {
+  const [text, setText] = useState('');
+  const [toDoList, setToDoList] = useState([]);
 
   // Creates new todo task
   function createTodo(_event) {
-    setToDoList([...toDoList, text]);
-    setCompleted([...completed, false]);
+    let todo = {
+      title: text,
+      complete: false
+    }
+    
+    setToDoList([...toDoList, todo]);
   }
 
   // saves text in input, stores the value in text state
@@ -25,59 +26,23 @@ export default function App() {
     setText(event.target.value);
   }
 
-  // switch complete state value
-  function changeCompleted(index) {
-    let newArray = completed.slice();
-    newArray[index] = !newArray[index];
-    
-    setCompleted(newArray);
-  }
-  
   // completes task and checkmarks the task for visible completion
-  function completeTask(event) {  
-    let btnElement = event.target.parentElement;
-    let pElement = btnElement.firstElementChild;
-    let textContent = pElement.innerHTML;
+  function completeTask(index) {  
+    let copyArray = [...toDoList];
+    let toDoAtIndex = copyArray[index];
+
+    toDoAtIndex.complete = !toDoAtIndex.complete;
     
-    let compIndex = toDoList.indexOf(textContent);
-    let newList = toDoList.slice();
-    
-    changeCompleted(compIndex);
-
-    // Figure out a way to run the change after the complete state runs 
-
-    // useEffect(() => {
-    //   if (completed[compIndex] === true) {
-    //     newList[compIndex] = '\u2713 --- ' + textContent + ' --- \u2713';
-    //     setToDoList(newList);
-    //     return;
-    //   }
-    //   else {
-    //     let newEle = newList[compIndex];
-    //     newEle = newEle.slice(6, -6);
-    //     newList[compIndex] = newEle;
-    //     setToDoList(newList);
-    //     return;
-    //   }
-    // }, [completed]);
-
-    if (completed[compIndex] === true) {
-      newList[compIndex] = '\u2713 --- ' + textContent + ' --- \u2713';
-      setToDoList(newList);
-      return;
-    }
-    else {
-      let newEle = newList[compIndex];
-      newEle = newEle.slice(6, -6);
-      newList[compIndex] = newEle;
-      setToDoList(newList);
-      return;
-    }
+    setToDoList(copyArray); 
   }
 
-  const [text, setText] = useState('');
-  const [toDoList, setToDoList] = useState([]);
-  const [completed, setCompleted] = useState([]);
+  let getObjectName = (obj) => {
+    return obj.complete ? true: false;
+  }
+  const completedTodos = toDoList.filter(getObjectName);
+  const numberOfCompletedTodos = completedTodos.length;
+
+  let textCount = text.length;
 
   return (
     <>
@@ -85,18 +50,30 @@ export default function App() {
         <h1>Todo List</h1>
         <div className="main">
           <h4 className="header">Add Todo</h4>
-          <input className="input" type="text" id="newInput" placeholder="Add New Todo" value={text} onChange={handleChange}/>
-          <button className="btn" onClick={createTodo}>Submit</button>
+          <input className="input" type="text" id="newInput" placeholder="Add New Todo" maxLength='120' value={text} onChange={handleChange}/>
+          <div id="submit-p-container">
+            <button className="btn" onClick={createTodo}>Submit</button>
+            <p id="char-count">{textCount} / 120</p>
+          </div>
           {
-            toDoList.map((inp)=> { // first argument is value, second argument is index, third arg is entire array
+            toDoList.map((inp, ind) => { // first argument is value, second argument is index, third arg is entire array
+
               return (
                 <div className="task-container"> 
-                  <p className="text" id="text-content">{inp}</p>
-                  <button className="check-btn" id="first" onClick={completeTask}>{checkBtn}</button>
-                  <button className="del-btn" id="second" onClick={()=>console.log('deleted task')}>{xBtn}</button>
+                  <p className="text" id="text-content">
+                    {
+                      inp.complete ? `\u2713 ---  ${inp.title}  --- \u2713` : inp.title                    
+                    }
+                  </p>
+                  <button className="check-btn" id="first" onClick={() => {completeTask(ind)}}>
+                    {checkBtn}
+                  </button>
+                  <button className="del-btn" id="second" onClick={ function() { console.log('deleted task') } }>{xBtn}</button>
                 </div>        
-              )})
+              );
+            })
           }
+          <p>Completed Todos: { numberOfCompletedTodos } </p>
         </div>
       </div>
     </>
--- src/show.jsx
+++
@@ -1,9 +0,0 @@
-// import Task from './task.jsx'
-
-// export default function Showtodo() {
-//     return (
-//         <div className="show-container" style={{marginTop: 25}}>
-//             <Task />
-//         </div>
-//     )
-// }
