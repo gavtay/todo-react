d003fb1d4e6a0c625d0a3570c1c2413c1f2fada0
Kataneki <gavintaylor.ai@gmail.com>
2023-11-11 22:02:31 -0600

    Broke the todo app into seperate components.

--- src/App.jsx
+++ src/App.jsx
@@ -8,62 +8,54 @@ const checkBtn = <FontAwesomeIcon id="check" icon={faCheck} style={{color: "#00f
 const xBtn = <FontAwesomeIcon id="x" icon={faX} style={{color: "#ff0000", pointerEvents: 'none'}} />
 
 export default function App() {
-  const [text, setText] = useState('');
-  const [filter, setFilter] = useState('');
   const [toDoList, setToDoList] = useState([]);
+  // const [filter, setFilter] = useState('');
+
+  // render specific tasks based on the filter
+  // function filterTodoChange(event) {
+  //   setFilter(event.target.value);
+  // }
+
+  // Show filtered tasks
+  // function renderTasks() {
+  //   let newArray = toDoList.slice();
+        
+  //   newArray = newArray.filter((todo) => {
+  //     let str = todo.title.toLowerCase();
+  //     let filt = filter.toLocaleLowerCase();
+
+  //     if (str.includes(filt)) {
+  //       return todo;
+  //     }
+  //   })
+
+  //   newArray = newArray.map((inp, ind, arr) => { // first argument is value, second argument is index, third arg is entire array
+  //     return (
+  //       <>
+  //         <div className="task-container"> 
+  //           <p className="text" id="text-content">
+  //             {
+  //               inp.complete ? `\u2713 ---  ${inp.title}  --- \u2713` : inp.title                    
+  //             }
+  //           </p>
+  //           <button className="check-btn" id="first" onClick={() => {completeTask(ind)}}>
+  //             {checkBtn}
+  //           </button>
+  //           <button className="del-btn" id="second" onClick={()=> {deleteTask(ind, arr)}}>{xBtn}</button>
+  //         </div>        
+  //       </>
+  //     );
+  //   })
+  //   return newArray;
+  // }
 
   // Creates new todo task
-  function createTodo(_event) {
+  function createTodo(text) {
     let todo = {
       title: text,
       complete: false
     }
-    
     setToDoList([...toDoList, todo]);
-    setText('');
-  }
-
-  // saves text in input, stores the value in text state
-  function addTaskChange(event) {
-    setText(event.target.value);
-  }
-
-  // render specific tasks based on the filter
-  function filterTodoChange(event) {
-    setFilter(event.target.value);
-  }
-
-  // Show filtered tasks
-  function renderTasks() {
-    let newArray = toDoList.slice();
-        
-    newArray = newArray.filter((todo) => {
-      let str = todo.title.toLowerCase();
-      let filt = filter.toLocaleLowerCase();
-
-      if (str.includes(filt)) {
-        return todo;
-      }
-    })
-
-    newArray = newArray.map((inp, ind, arr) => { // first argument is value, second argument is index, third arg is entire array
-      return (
-        <>
-          <div className="task-container"> 
-            <p className="text" id="text-content">
-              {
-                inp.complete ? `\u2713 ---  ${inp.title}  --- \u2713` : inp.title                    
-              }
-            </p>
-            <button className="check-btn" id="first" onClick={() => {completeTask(ind)}}>
-              {checkBtn}
-            </button>
-            <button className="del-btn" id="second" onClick={()=> {deleteTask(ind, arr)}}>{xBtn}</button>
-          </div>        
-        </>
-      );
-    })
-    return newArray;
   }
 
   // completes task and checkmarks the task for visible completion
@@ -88,15 +80,65 @@ export default function App() {
     setToDoList(newToDoArray);
   }
 
-  // delete all completed tasks
-  function deleteCompletedTasks() {
-    let newArray = toDoList.slice();
+    // delete all completed tasks
+    function deleteCompletedTasks() {
+      let newArray = toDoList.slice();
+  
+      newArray = newArray.filter((toDo) => {
+        return toDo.complete !== true;
+      })
+      setToDoList(newArray);
+    }
 
-    newArray = newArray.filter((toDo) => {
-      return toDo.complete !== true;
-    })
+  return (
+    <>
+      <div className="container">
+        <h1>Todo List</h1>
+        <div className="main">
+          <AddToDo createTodo={createTodo}/>
+          {/* <div className='filterTasks'>
+            <h4 id='filter-input-header'>Filter Tasks by Name</h4>
+            <input
+              className="input"
+              type="text"
+              id="filter-input"
+              placeholder="Add New Todo"
+              maxLength='120'
+              value={filter}
+              onChange={filterTodoChange}
+            />
+          </div> */}
+          <div className='tasks-container'>
+            <h4 id='task-header'>Tasks</h4>
+            {
+              toDoList.map((inp, ind, arr) =>
+                <ShowToDoList 
+                  value={inp} 
+                  index={ind} 
+                  array={arr}
+                  completeTask={completeTask}
+                  deleteTask={deleteTask}
+                />
+              )
+            }
+          </div>
+          <ChangeCompletedToDo 
+            toDoList={toDoList} 
+            deleteCompletedTasks={deleteCompletedTasks}
+          />
+        </div>
+      </div>
+    </>
+  );
+}
 
-    setToDoList(newArray);
+export function AddToDo({createTodo}) {
+  const [text, setText] = useState('');
+  let textCount = text.length;
+
+  // saves text in input, stores the value in text state
+  function addTaskInputChange(event) {
+    setText(event.target.value);
   }
 
   // Toggle the view for the submit button
@@ -105,60 +147,70 @@ export default function App() {
       return null;
     }
     else {
-      return <button id="submit-btn" onClick={createTodo}>Submit</button>
+      return <button id="submit-btn" onClick={()=> {
+        createTodo(text);
+      }}>Submit</button>
     }
   }
 
-  // Toggle the completed todo count and delete completed buttons
-  function changeCompletedTodoVisibility() {
-    if (toDoList.length === 0) {
-      return null;
-    }
-    else {
-      return (
-        <>
-          <p>Completed Todos: </p>
-          <p id='amt-completed'>{numberOfCompletedTodos}</p>
-          <button id='del-completed' onClick={()=>{deleteCompletedTasks()}}>Delete Completed</button>
-        </>
-      )
-    }
-  }
+  return (
+    <>
+      <h4 className="header">Add Todo</h4>
+      <input
+        className="input"
+        type="text"
+        id="newInput"
+        placeholder="Add New Todo"
+        maxLength='120'
+        value={text}
+        onChange={addTaskInputChange}
+      />
+      <div id="submit-p-container">
+        <p id="char-count">{textCount} / 120</p>
+        {submitButtonVisibility()}
+      </div>
+    </>
+  )
+}
 
+export function ShowToDoList({ value, index, array, completeTask, deleteTask}) {
+  return (
+    <>
+      <div className="task-container"> 
+        <p className="text" id="text-content">
+          {
+            value.complete ? `\u2713 ---  ${value.title}  --- \u2713` : value.title                    
+          }
+        </p>
+        <button className="check-btn" id="first" onClick={() => {completeTask(index)}}>
+          {checkBtn}
+        </button>
+        <button className="del-btn" id="second" onClick={()=> {deleteTask(index, array)}}>{xBtn}</button>
+      </div>        
+    </>
+  )
+}
+
+export function ChangeCompletedToDo({ toDoList, deleteCompletedTasks }) {
   // Show # of completed todos
   let getObjectName = (obj) => {
     return obj.complete ? true: false;
   }
   const completedTodos = toDoList.filter(getObjectName);
-  const numberOfCompletedTodos = completedTodos.length;
+  const completedCount = completedTodos.length;
 
-  let textCount = text.length;
+  if (completedCount === 0) {
+    return null;
+  }
 
   return (
     <>
-      <div className="container">
-        <h1>Todo List</h1>
-        <div className="main">
-          <h4 className="header">Add Todo</h4>
-          <input className="input" type="text" id="newInput" placeholder="Add New Todo" maxLength='120' value={text} onChange={addTaskChange}/>
-          <div id="submit-p-container">
-            <p id="char-count">{textCount} / 120</p>
-            {submitButtonVisibility()}
-          </div>
-          <div className='filterTasks'>
-            <h4 id='filter-input-header'>Filter Tasks by Name</h4>
-            <input className="input" type="text" id="filter-input" placeholder="Add New Todo" maxLength='120' value={filter} onChange={filterTodoChange}/>
-          </div>
-          <div className='tasks-container'>
-            <h4 id='task-header'>Tasks</h4>
-            {renderTasks()}
-          </div>
-          <div id='completed-container'>
-            {changeCompletedTodoVisibility()}
-          </div>
-        </div>
+      <div id='completed-container'>
+        <p>Completed Todos: </p>
+        <p id='amt-completed'>{completedCount}</p>
+        <button id='del-completed' onClick={()=>{deleteCompletedTasks()}}>Delete Completed</button>
       </div>
     </>
-  );
+  )
 }
 
